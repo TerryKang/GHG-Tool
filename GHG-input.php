7fce3941be06b7383959f94e6807a463999e50d1
Isaac <isaac@isaacmorneau.com>
2016-11-20 01:12:26 +0000

    WIP moving to different computer

--- GHG-input.php
+++ GHG-input.php
@@ -4,14 +4,15 @@ function getLastScenario($con){
     $result = array(
         "facility"=>getDestination($con),
         "trucks"=>getTrucks($con),
-        "comps"=>remKey(getComposition($con)),
+        "comps"=>getComposition($con),
         "results"=>array()
     );
     $source = getSource($con);
     foreach($source as $single){
         $result["results"][] = array(
             "label"=>$single,
-            "data"=>array(1000,55,10,2,29,4),//add actual data
+            "tonnage"=>10000,//todo add -> getSourceTonnage($con),
+            "data"=>getDataBySource($con,$single),//array(55,10,2,29,4),//add actual data
             "dest"=>array(
                 array(
                 "facility"=>0,
@@ -23,20 +24,34 @@ function getLastScenario($con){
     //case insenstive functions are amusing
     return jSoN_EnCoDe($result);
 }
-function remKey($tmp){ 
-    $tmp['key'] = null;
-    return $tmp;
-}
 function getTrucks($con){
-    $sql = "SELECT model FROM Vehicle";
+    $sql = "SELECT modelId, model FROM Vehicle";
     $stmt = sqlsrv_query( $con, $sql );
     if( $stmt === false) {
         die( print_r( sqlsrv_errors(), true) );
     }
     $model = array();
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-        $model[]=$row['model'];
+        $model[$row['modelId']]=$row['model'];
     }
+    $model['key'] = 'modelId';
     return $model;
 }
 
+function getDataBySource($con, $source ){
+    $sql = 
+        "SELECT compositionId, tonnageWT FROM SourceByComp "
+            ."WHERE sourceId = (SELECT sourceId FROM Source WHERE sourceName = $source) "
+            ."GROUP BY sourceId";//its ok but not good to use string insertion here because this is an internal only 
+                //call without user interaction otherwise it would be a major injection point
+    $stmt = sqlsrv_query( $con, $sql );
+    if( $stmt === false) {
+        die( print_r( sqlsrv_errors(), true) );
+    }
+    $compData = array();
+    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+        $compData[$row['compositionId']]=$row['tonnageWT'];
+    }
+    $compData['key'] = 'compositionId';
+    return $compData;
+}
